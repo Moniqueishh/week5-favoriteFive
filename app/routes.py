87d2371dd54ef2648b16ed9a067bede956a4293b
Monique Vargas <moniquev432@yahoo.com>
2023-03-25 12:18:45 -0500

    first commit

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+
+from app import app
+
+from flask import render_template
+
+
+@app.route('/')
+def homePage():
+    return render_template('index.html')
+
+@app.route('/favoritefive')
+def favoriteFive():
+    favorite = [
+            {
+            "name" : "The 1975"
+            },
+            {
+            "name" : "Hozier"
+            },
+            {
+            "name" : "Jesse Reyez"
+            },
+            {
+            "name" : "Omar Apollo"
+            },
+            {
+            "name" : "Kali Uchis"
+            }
+        ]
+    return render_template("favoritefive.html", favorite=favorite)
+
+# @app.route('/login')
+# def loginPage():
+#     return render_template('login.html')
